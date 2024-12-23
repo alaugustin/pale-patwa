ef3fca0d590d83244ab7259cd1af27833b3ab2cd
Al Augustin <aaug@caasco.ca>
2024-12-23 16:34:26 -0500

    feat(modal): integrate dictionary data and enhance example sentences
    - Add SortedDictionary integration for word lookup in Modal
    - Update data structure to split example sentences into Kweyol and English
    - Refactor word attributes to use selected word data
    - Enhance display format with definition and bilingual examples

--- src/Components/Organisms/Modal/Modal.tsx
+++ src/Components/Organisms/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SortedDictionary } from '../../../Data/data';
 import { BlockElement } from '../../UI/BlockLevel/BlockElement';
 import { Typography } from '../../Typography/Typography';
 import WordAttributes from '../WordAttributes/WordAttributes';
@@ -8,6 +9,7 @@ export default function Modal({
   modalTitle,
   clickHandler
 }: IModalProps) {
+  const selectedWord = SortedDictionary.find(word => word.word === modalTitle);
 
   return (
     <BlockElement variant='article' className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
@@ -15,17 +17,21 @@ export default function Modal({
         <Typography variant='h2' className="text-xl font-bold mb-4">{modalTitle}</Typography>
         <BlockElement className='mb-4'>
           <Typography variant='p' className='mb-4'>
-            {'N Monday Mwen hayi alé twavay lé Lendi-a'}
+            <Typography variant='p'>{
+              selectedWord?.definition}&nbsp;
+            {<><strong>***Kw: Lorem ipsum dolor sit amet, consectetur adipiscing elit***</strong></>}&nbsp;
+            {<>En: ***Lorem ipsum dolor sit amet, consectetur adipiscing elit***</>}
+            </Typography>
           </Typography>
 
           <WordAttributes
-            wordEtymology={'[< Fr. lundi]'}
-            wordPartOfSpeech={'N'}
-            wordDialect={'🇱🇨'}
-            borderClasses={'borderClasses'}
+            wordEtymology={selectedWord?.etymology ?? null}
+            wordPartOfSpeech={selectedWord?.partOfSpeech ?? null}
+            wordDialect={selectedWord?.dialect ?? null}
             containerClasses='text-sm mb-8'
           />
         </BlockElement>
+
         <button
           onClick={clickHandler}
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
