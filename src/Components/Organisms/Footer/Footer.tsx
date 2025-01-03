febcf01e5ec40a51914053efcabf51bb1fd3b3f3
Al Augustin <aaug@caasco.ca>
2025-01-03 10:29:31 -0500

    feat: enhance app configuration and UI components
    - Add homepage and module type to package.json
    - Update TypeScript version
    - Add SVG favicon with gradient design
    - Enhance meta tags and SEO optimization
    - Add Google Analytics integration
    - Improve Footer component with resources section
    - Refine WordAttributes styling and border handling
    - Implement responsive pagination based on viewport height
    - Update UI styling classes and add PDF icon

--- src/Components/Organisms/Footer/Footer.tsx
+++ src/Components/Organisms/Footer/Footer.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { AppContentData } from '../../../Data/AppContent';
 import { BlockElement } from '../../UI/BlockLevel/BlockElement';
+import { Typography } from '../../UI/Typography/Typography';
 import { IFooterProps } from './Footer.d';
 
-const { footerInfoClasses } = AppContentData.uiClasses;
-const { copyRight } = AppContentData.icons;
+const { copyRight, pdfIcon } = AppContentData.icons;
 
 export default function Footer({
   footerContainerClass,
@@ -15,7 +15,18 @@ export default function Footer({
 
   return (
     <BlockElement variant='footer' className={footerContainerClass}>
-      <BlockElement className={footerInfoClasses}>{copyRight}&nbsp;{footerInfo}</BlockElement>
+      <BlockElement className='flex flex-row'>{copyRight}&nbsp;{footerInfo}</BlockElement>
+      <BlockElement className='flex flex-col'>
+        <Typography variant='h3' className='font-bold'>{'Resources'}</Typography>
+        <Typography variant='p' className={'mb-2'}>
+          <a href='http://www.saintluciancreole.dbfrank.net/dictionary/KweyolDictionary.pdf' target='_blank' rel='noreferrer noopener'>{'Kwéyòl Dictionary'}</a> - Ministry of Education, Government of Saint Lucia
+          <Typography variant='span'>{pdfIcon}</Typography>
+        </Typography>
+        <Typography variant='p' className={'mb-2'}>
+          <a href='https://www.webonary.org/kweyol/' target='_blank' rel='noreferrer noopener'>{'Kwéyòl Dictionary'}</a> - Webonary
+        </Typography>
+      </BlockElement>
+      <BlockElement className='flex flex-row'>🇱🇨🇩🇲🇬🇩🇹🇹🇭🇹🇬🇫🇻🇪🇻🇨</BlockElement>
     </BlockElement>
   );
 }
